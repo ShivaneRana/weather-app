aa2b1ad88a14f001a3f307dbfdef7ee0f08431e2
Shivane Rana <shivanerana2004@gmail.com>
2024-11-17 13:49:13 +0530

    api.js was created and minor stype fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+
+const searchName = document.querySelector(".searchName");
+const searchButton = document.querySelector(".searchButton")
+
+searchName.addEventListener("input",() => {
+    console.log(searchName.value);
+})
