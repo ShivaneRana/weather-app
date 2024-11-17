5ed66721838fa79c97e5bd9a7bc7b40af35e1eff
Shivane Rana <shivanerana2004@gmail.com>
2024-11-17 16:14:35 +0530

    writing logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import "./style.css";
 
 const searchName = document.querySelector(".searchName");
 const searchButton = document.querySelector(".searchButton")
+const location = document.querySelector(".location");
+const currentDate = document.querySelector(".currentDate");
+const currentDay = document.querySelector(".currentDay");
+const temperature = document.querySelector(".temperature");
+const image = document.querySelector(".image");
+const unit = document.querySelector(".unit");
+
 
 searchName.addEventListener("input",() => {
+
+})
+
+searchButton.addEventListener("click",() => {
     console.log(searchName.value);
+})
+
+unit.addEventListener("input",() => {
+    console.log(unit.value)
 })
