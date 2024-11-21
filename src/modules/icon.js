8ed506fd3994fdc5b195aace17b64c684ca7ce53
Shivane Rana <shivanerana2004@gmail.com>
2024-11-21 20:00:34 +0530

    icon js import all the weatherIcon path as object

---
+++ src/modules/icon.js
@@ -0,0 +1,47 @@
+import clearDay from "../asset/images/weatherIcon/clear-day.svg";
+import clearNight from "../asset/images/weatherIcon/clear-night.svg";
+import cloudy from "../asset/images/weatherIcon/cloudy.svg";
+import fog from "../asset/images/weatherIcon/fog.svg";
+import hail from "../asset/images/weatherIcon/hail.svg";
+import partlyCloudyDay from "../asset/images/weatherIcon/partly-cloudy-day.svg";
+import partlyCloudyNight from "../asset/images/weatherIcon/partly-cloudy-night.svg";
+import rain from "../asset/images/weatherIcon/rain.svg";
+import rainSnow from "../asset/images/weatherIcon/rain-snow.svg";
+import rainSnowShowersDay from "../asset/images/weatherIcon/rain-snow-showers-day.svg";
+import rainSnowShowersNight from "../asset/images/weatherIcon/rain-snow-showers-night.svg";
+import showersDay from "../asset/images/weatherIcon/showers-day.svg";
+import showersNight from "../asset/images/weatherIcon/showers-night.svg";
+import sleet from "../asset/images/weatherIcon/sleet.svg";
+import snow from "../asset/images/weatherIcon/snow.svg";
+import snowShowersDay from "../asset/images/weatherIcon/snow-showers-day.svg";
+import snowShowersNight from "../asset/images/weatherIcon/snow-showers-night.svg";
+import thunder from "../asset/images/weatherIcon/thunder.svg";
+import thunderRain from "../asset/images/weatherIcon/thunder-rain.svg";
+import thunderShowersDay from "../asset/images/weatherIcon/thunder-showers-day.svg";
+import thunderShowersNight from "../asset/images/weatherIcon/thunder-showers-night.svg";
+import wind from "../asset/images/weatherIcon/wind.svg";
+
+export const icons = {
+  "clear-day": clearDay,
+  "clear-night": clearNight,
+  cloudy: cloudy,
+  fog: fog,
+  hail: hail,
+  "partly-cloudy-day": partlyCloudyDay,
+  "partly-cloudy-night": partlyCloudyNight,
+  rain: rain,
+  "rain-snow": rainSnow,
+  "rain-snow-showers-day": rainSnowShowersDay,
+  "rain-snow-showers-night": rainSnowShowersNight,
+  "showers-day": showersDay,
+  "showers-night": showersNight,
+  sleet: sleet,
+  snow: snow,
+  "snow-showers-day": snowShowersDay,
+  "snow-showers-night": snowShowersNight,
+  thunder: thunder,
+  "thunder-rain": thunderRain,
+  "thunder-showers-day": thunderShowersDay,
+  "thunder-showers-night": thunderShowersNight,
+  wind: wind,
+};
